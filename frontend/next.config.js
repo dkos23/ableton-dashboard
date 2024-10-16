da3e2ff5e8c003866e861d69d3bb6182c46ccfa8
DESKTOP-T78J8K3\dc <dalibor.kos23@gmail.com>
2024-10-16 20:00:30 +0200

    update next.config and revert nextjs.yaml next export

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  // Optionally, if your app is hosted on a subpath like https://<username>.github.io/<repo>/
-  // If not, you can omit this or leave it empty
-  basePath: '/ableton-dashboard',
+  output: 'export',  // Tells Next.js to output static files
   images: {
-    unoptimized: true, // Necessary if you're using Next.js images on GitHub Pages
+    unoptimized: true, // For static sites, disables Next.js' image optimization
   },
+  // Add basePath if needed for GitHub Pages hosting
+  basePath: '/ableton-dashboard',
 };
 
 module.exports = nextConfig;
