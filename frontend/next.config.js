2f7ce3b0d105efc1be101caaa566dc23598fd30d
DESKTOP-T78J8K3\dc <dalibor.kos23@gmail.com>
2024-10-16 19:57:13 +0200

    add next eport and basePath to gihub repo

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,12 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  output: 'export',
+  // Optionally, if your app is hosted on a subpath like https://<username>.github.io/<repo>/
+  // If not, you can omit this or leave it empty
+  basePath: '/ableton-dashboard',
+  images: {
+    unoptimized: true, // Necessary if you're using Next.js images on GitHub Pages
+  },
+};
 
 module.exports = nextConfig;
